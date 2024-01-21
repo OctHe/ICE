73b79a4f49254e3c51366b215135848b38871eb8
OctHe <hsy1995313@gmail.com>
2024-01-21 17:30:07 +0800

    Update options

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,59 +1,52 @@
 -- ---------------------------------------------------------------------
 --
--- GL neovim run command file. It uses Lazy.nvim as the plugin manager.
+-- GL neovim run command file. It uses Lazy.nvim.
 -- Author: OctHe
--- Copyright (C): 2024
+-- Copyright (C): 2023-2024
 --
 -- ---------------------------------------------------------------------
 
--- View {{{1
-vim.o.list = true   -- Show tabs as ">", trailing spaces as "-", and non-breakable space characters as "+". 
+local opt = vim.opt
 
-vim.o.number = true
-vim.o.wrap = true
-vim.o.linebreak = true
-vim.o.showbreak = ">>"
-vim.o.mouse = "a"
-vim.o.termguicolors = true
-vim.o.signcolumn = "yes"
-vim.o.cursorline = true
-vim.o.scrolloff = 5
-vim.o.sidescrolloff = 5
--- }}}1
+opt.list = true   -- Shoptw tabs as ">", trailing spaces as "-", and noptn-breakable space characters as "+". 
+opt.number = true
+opt.wrap = true
+opt.linebreak = true
+opt.showbreak = ">>"
+opt.mouse = "a"
+opt.termguicolors = true
+opt.signcolumn = "yes" -- Always show the signcolumn
+opt.cursorline = true -- Highlight the current line
+opt.scrolloff = 4
+opt.sidescrolloff = 4
+opt.expandtab = true -- Use space instead of tab
+opt.softtabstop = 4
+opt.shiftwidth = 4
+opt.undofile = true
+opt.smartindent = true
+opt.autowrite = true
+opt.clipboard = "unnamedplus" -- Sync with system-level clipboard
 
--- Completion {{{1
-vim.o.expandtab = true
-vim.o.softtabstop = 4
-vim.o.shiftwidth = 4
-vim.o.undofile = true
+if vim.fn.has("nvim-0.10") then
+  opt.smoothscroll = true
+end
 
-vim.o.smartindent = true
--- }}}1
-
--- Keymapping {{{1
 vim.g.mapleader = " "
-vim.g.maplocalleader = " "
--- }}}1
+vim.g.maplocalleader = "\\"
 
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
 end
-vim.opt.rtp:prepend(lazypath)
+opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
     spec = {
         { import = "plugins" },
     },
-    install = { colorscheme = { "tokyonight", "habamax" } },
+    -- try to load one of these colorschemes when starting an installation during startup
+    install = { colorscheme = { "tokyonight" } },
     checker = { enabled = true }, -- automatically check for plugin updates
     performance = {
     rtp = {
