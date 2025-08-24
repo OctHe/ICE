4a1b8b18663e6f7f4599da05b484af5c43244607
Shiyue <hsy1995313@gmail.com>
2025-08-24 22:04:42 +0800

    Fix: Remove ftplugin for help in nvim/nvimini

--- .config/nvim/ftplugin/help.lua
+++
@@ -1,5 +0,0 @@
-vim.o.number = true
-if vim.fn.winwidth("%d") > 180 then
-    vim.cmd[[ wincmd L ]]
-    vim.cmd[[ vertical resize 90 ]]
-end
--- .config/nvimini/ftplugin/help.lua
+++
@@ -1,5 +0,0 @@
-vim.o.number = true
-if vim.fn.winwidth("%d") > 180
-    vim.cmd[[ wincmd L ]]
-    vim.cmd[[ vertical resize 90 ]]
-endif
